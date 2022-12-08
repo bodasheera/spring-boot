041bec2a00de8614a126df323e5a1edce3fd51a6
Vignesh Saikrishna Mallya <vignesh@appveen.com>
2022-12-08 16:07:25 +0530

    class equivalent of record

--- src/main/java/com/viggicodes/Main.java
+++ src/main/java/com/viggicodes/Main.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @SpringBootApplication
 @RestController // part of spring web mvc
 public class Main {
@@ -17,5 +19,41 @@ public class Main {
         return new GreetResponse("Hello");
     }
 
-    record GreetResponse(String greet){} // its basically like getter / setter for a variable
+//    record GreetResponse(
+//            String greet
+//    ){} // its basically creates a class with member as greet . immutable. in built getter and setter for this member
+
+    class GreetResponse {
+        private final String greet;
+
+
+        GreetResponse(String greet) {
+            this.greet = greet;
+        }
+
+        @Override
+        public String toString() {
+            return "GreetResponse{" +
+                    "greet='" + greet + '\'' +
+                    '}';
+        }
+
+        public String getGreet() {
+            return greet;
+        }
+
+        // use alt + insert for ggenerate equals and hashcode
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            GreetResponse that = (GreetResponse) o;
+            return Objects.equals(greet, that.greet);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(greet);
+        }
+    }
 }
