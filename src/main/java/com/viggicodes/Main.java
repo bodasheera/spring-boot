db1b445017fe956d649ce7b64d7d14e9f64bbf15
Vignesh Saikrishna Mallya <vignesh@appveen.com>
2022-12-08 15:31:23 +0530

    added record

--- src/main/java/com/viggicodes/Main.java
+++ src/main/java/com/viggicodes/Main.java
@@ -6,14 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+@RestController // part of spring web mvc
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
-    @GetMapping("/greet")
-    public String greet() {
-        return "Hello";
+    @GetMapping("/greet")  // part of mvc
+    public GreetResponse greet() {
+        return new GreetResponse("Hello");
     }
+
+    record GreetResponse(String greet){} // its basically like getter / setter for a variable
 }
