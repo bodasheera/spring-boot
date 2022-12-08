f017bd71918cca3284bd4e36f67191dfb6abdfbe
Vignesh Saikrishna Mallya <vignesh@appveen.com>
2022-12-08 17:18:51 +0530

    db config

--- src/main/java/com/viggicodes/Main.java
+++ src/main/java/com/viggicodes/Main.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Objects;
+import java.util.List;
 
 @SpringBootApplication
 @RestController // part of spring web mvc
@@ -16,44 +16,55 @@ public class Main {
 
     @GetMapping("/greet")  // part of mvc
     public GreetResponse greet() {
-        return new GreetResponse("Hello");
+        GreetResponse response = new GreetResponse(
+                "Hello",
+                List.of("Java", "Golang", "Javascript"),
+                new Person("Vignesh", 23,30_000)
+        );
+        return response;
     }
 
-//    record GreetResponse(
-//            String greet
-//    ){} // its basically creates a class with member as greet . immutable. in built getter and setter for this member
+    record Person(String name, int age, double savings) {
 
-    class GreetResponse {
-        private final String greet;
-
-
-        GreetResponse(String greet) {
-            this.greet = greet;
-        }
-
-        @Override
-        public String toString() {
-            return "GreetResponse{" +
-                    "greet='" + greet + '\'' +
-                    '}';
-        }
-
-        public String getGreet() {
-            return greet;
-        }
-
-        // use alt + insert for ggenerate equals and hashcode
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            GreetResponse that = (GreetResponse) o;
-            return Objects.equals(greet, that.greet);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(greet);
-        }
     }
+    record GreetResponse(
+            String greet,
+            List<String> favProgrammingLanguages,
+            Person person
+    ){} // its basically creates a class with member as greet . immutable. in built getter and setter for this member
+
+//    class GreetResponse {
+//        private final String greet;
+//
+//
+//        GreetResponse(String greet) {
+//            this.greet = greet;
+//        }
+//
+//        @Override
+//        public String toString() {
+//            return "GreetResponse{" +
+//                    "greet='" + greet + '\'' +
+//                    '}';
+//        }
+//
+//        public String getGreet() {
+//            return greet;
+//        }
+//
+//        // use alt + insert for ggenerate equals and hashcode
+//        @Override
+//        public boolean equals(Object o) {
+//            if (this == o) return true;
+//            if (o == null || getClass() != o.getClass()) return false;
+//            GreetResponse that = (GreetResponse) o;
+//            return Objects.equals(greet, that.greet);
+//        }
+//
+//        @Override
+//        public int hashCode() {
+//            return Objects.hash(greet);
+//        }
+//    }
+
 }
