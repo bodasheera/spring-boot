1c2435ef7a6473ce0528b24f83a384d47c9ccae3
Vignesh Saikrishna Mallya <vignesh@appveen.com>
2022-12-10 10:59:35 +0530

    crud operations

--- src/main/java/com/viggicodes/Main.java
+++ src/main/java/com/viggicodes/Main.java
@@ -2,14 +2,22 @@ package com.viggicodes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootApplication
 @RestController // part of spring web mvc
+@RequestMapping("api/v1/customers")
 public class Main {
+
+    private final CustomerRepository customerRepository; // dependency injection same as angular
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
@@ -67,4 +75,37 @@ public class Main {
 //        }
 //    }
 
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerRepository.findAll();
+    }
+
+    record NewCustomerRequest(Integer age, String name, String email){
+
+    }
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request){
+        Customer customer = new Customer();
+        customer.setAge(request.age());
+        customer.setEmail(request.email());
+        customer.setName(request.name());
+
+        customerRepository.save(customer);
+    }
+
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id){
+        customerRepository.deleteById(id);
+    }
+
+    @PutMapping("{customerId}")
+    public void updateCustomer(@PathVariable("customerId") Integer id, @RequestBody NewCustomerRequest request){
+        Customer customer = customerRepository.findById(id).get();
+        customer.setName(request.name());
+        customer.setEmail(request.email());
+        customer.setAge(request.age());
+
+        customerRepository.save(customer);
+    }
 }
